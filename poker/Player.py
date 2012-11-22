74b4eff1d587274dc0f9ad1413c95c4a477bfd33
Nikolay Karpov <nk-karpov@yandex-team.ru>
2012-11-22 22:32:23 +0400

    add empty bot

--- poker/Player.py
+++ poker/Player.py
@@ -1,54 +1,49 @@
 __author__ = 'nk-karpov'
 
 class Player(object):
+    __many = 0
+    __hand = 0
+    __small_blind = 0
+    __big_blind = 0
+    __ante = 0
+    __name = None
 
-    def __init__(self):
-        self.__many = 0
-        self.__hand = 0
-        self.__small_blind = 0
-        self.__big_blind = 0
-        self.__ante = 0
-        self.__log_turn = []
-        self.__name = "empty bot"
+    def __init__(self, name):
+        assert name is str
+        __name = name
 
-    def __check__(self):
-        return ["check", None]
+    def __check_or_call__(self):
+        pass #TODO protocol
 
     def __fold__(self):
-        return ["fold", None]
+        pass #TODO protocol
 
     def __raise__(self, value):
-        if value is not int:
-            RuntimeError()
-        return ["raise", value]
+        pass #TODO protocol
 
     def turn(self, value):
-        pass
+        pass #TODO override
 
     def add_many(self, value):
         self.__many += value
 
-    def set_blinds(self, small_blinds_value):
-        self.__small_blind = small_blinds_value
-        self.__big_blind = 2 * small_blinds_value
+    def set_blinds(self, small_blind_value, big_blind_value):
+        self.__small_blind = small_blind_value
+        self.__big_blind = big_blind_value
 
     def set_ante(self, ante_value):
         self.__ante = ante_value
 
-    def small_blind(self):
-        result = min(self.__many, self.small_blind)
-        self.__many -= self.__small_blind
-        return result
+    def put_small_blind(self):
+        self.__many -= min(self.__many, self.__small_blind)
 
-    def big_blind(self):
+    def put_big_blind(self):
         result = min(self.__many, self.__big_blind)
         self.__many -= self.__big_blind
         return result
 
-    def ante(self):
-        result = min(self.__many, self.__ante)
-        self.__many -= self.__ante
-        return result
+    def put_ante(self):
+        self.__many -= min(self.__many, self.__ante)
 
     def many(self):
         return self.__many
@@ -56,8 +51,15 @@ class Player(object):
     def set_hand(self, hand):
         self.__hand = hand
 
-    def add_log_turn(self, log_turn):
-        self.__log_turn.append(log_turn)
+    def add_message(self, message):
+        pass #TODO override and protocol all logica with analyzer and gui
 
-    def new_round(self):
+    def wait(self):
+        #TODO protocol
+        #wait message
+        #while message is not broadcast:
+        #   add_message(message)
+        #   continue wait
+        #else:
+        #   break wait
         pass
