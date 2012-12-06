2af3b6a9e7131acd54f6de74965fd8d75aca7a64
Nikolay Karpov <nk-karpov@yandex-team.ru>
2012-12-07 03:16:01 +0400

    fix

--- poker/Bot.py
+++ poker/Bot.py
@@ -22,9 +22,29 @@ class Bot(Player):
 #		log_turn = table_info.turns()
 #		result = 0
 # 		for turn in table:
-#			if turn.
-	def turn_preflop(self):
-		count_raise = self.count_raise()
+#			if turn.verdict == 'raise':
+#				result += 1
+#		return result
+	def btc(self, hand_mask, enemy_mask, open_mask):
+		num_rem_cards = 5 - bit_count(open_mask)
+		if num_rem_cards == 0:
+			tmp = cmp_hand(get_list_cards_from_mask(hand_mask | open_mask),
+						get_list_cards_from_mask(enemy_mask | open_mask))
+			if tmp == -1:
+				return 1.
+			else:
+				return 0.
+		total_mask = open_mask | enemy_mask | open_mask
+		result = 0.
+		for i in xrange(len(suits) * len(values)):
+			if (total_mask >> i) & 1 == 1:
+				continue
+			result += self.go(hand_mask, enemy_mask, open_mask | (1 << i))
+		result /= but
+
+	def turn_preflop(self, value):
+#		count_raise = self.count_raise()
+		count_raise = 1
 		position = self.table_info.get_position(self.store.name())
 		mask = get_mask_of_cards(self.store.hand_cards())
 		aa = bit_count(mask & value_masks['a']) == 2
@@ -39,44 +59,130 @@ class Bot(Player):
 			elif count_raise == 1:
 				return self.__raise__(self.table_info.big_blind * 4)
 			else:
-				return self.__allin__()
+				return self.__allin__(value)
 		if qq:
 			if count_raise == 0:
 				return self.__raise__(self.table_info.big_blind * 4)
 			elif count_raise == 1:
 				return self.__raise__(self.table_info.big_blind * 4)
 			else:
-				return self.__check_or_call__()
+				return self.__check_or_call__(value)
 		if ak:
 			if count_raise == 0:
-				return self.__raise__(self.table_info.big_blind * 4)
+				return self.__raise__(self.table_info.big_blind * 4 + value)
 			elif count_raise ==  1:
-				return self.__check_or_call__()
+				return self.__check_or_call__(value)
 			else:
 				return self.__fold__()
 		if jj | tt:
 			if count_raise == 0:
-				if position < 4:
-					return self.__check_or_call__()
+				if 2 <= position and position <= 5:
+					return self.__check_or_call__(value)
 				else:
-					return self.__raise__(self.table_info.big_blind * 4)
+					return self.__raise__(self.table_info.big_blind * 4 + value)
 			else:
 				return self.__fold__()
-		return self.__fold__()
+		for value in values:
+			if bit_count(mask & value_masks[value]):
+				if count_raise == 0:
+					self.__check_or_call__(value)
+				else:
+					self.__fold__()
+		if bit_count(mask & value_masks['a']) == 1 and bit_count(mask & value_masks['j']):
+			if count_raise == 0:
+				if position <= 5:
 
-	def turn_flop(self):
+				else:
+					return self.__raise__(4 * self.table_info.big_blind * 4 + value)
+			else:
+				return self.__fold__()
 
-		return self.__check_or_call__()
-	def turn(self):
+
+	def turn_flop(self, value):
+		total = len(suits) * len(values)
+		mult = 2. / (total - 5) / (total - 6)
+		hand_mask = get_mask_of_cards(self.store.hand_cards())
+		opened_mask = get_mask_of_cards(self.store.opened_cards())
+		probablity = 0.
+		for i in xrange(52):
+			if hand_mask & (1 << i) != 0 or opened_mask & (1 << i) != 0:
+				continue
+			for j in xrange(i):
+				if hand_mask & (1 << i) != 0 or opened_mask & (1 << i) != 0:
+					continue
+				enemy_mask = (1 << i) | (1 << j)
+				probablity += self.go(hand_mask, enemy_mask, opened_mask)
+		probablity *= mult
+		if probablity > 0.75:
+			return self.__raise__(self.table_info.big_blind() * 4 + value)
+		elif probablity > 0.25:
+			return self.__check_or_call__(value)
+		else:
+			if value > 0:
+				return self.__fold__()
+			else:
+				return self.__check_or_call__(value)
+
+	def turn_turn(self, value):
+		total = len(suits) * len(values)
+		mult = 2. / (total - 6) / (total - 7)
+		hand_mask = get_mask_of_cards(self.store.hand_cards())
+		opened_mask = get_mask_of_cards(self.store.opened_cards())
+		probability = 0.
+		for i in xrange(52):
+			if hand_mask & (1 << i) != 0 or opened_mask & (1 << i) != 0:
+				continue
+			for j in xrange(i):
+				if hand_mask & (1 << i) != 0 or opened_mask & (1 << i) != 0:
+					continue
+				enemy_mask = (1 << i) | (1 << j)
+				probability += self.btc(hand_mask, enemy_mask, opened_mask)
+		probability *= mult
+		if probability > 0.75:
+			return self.__raise__(self.table_info.big_blind() * 4 + value)
+		elif probability > 0.25:
+			return self.__check_or_call__(value)
+		else:
+			if value > 0:
+				return self.__fold__()
+			else:
+				return self.__check_or_call__(value)
+
+	def turn_river(self, value):
+		total = len(suits) * len(values)
+		mult = 2. / (total - 7) / (total - 8)
+		hand_mask = get_mask_of_cards(self.store.hand_cards())
+		opened_mask = get_mask_of_cards(self.store.opened_cards())
+		probability = 0.
+		for i in xrange(52):
+			if hand_mask & (1 << i) != 0 or opened_mask & (1 << i) != 0:
+				continue
+			for j in xrange(i):
+				if hand_mask & (1 << i) != 0 or opened_mask & (1 << i) != 0:
+					continue
+				enemy_mask = (1 << i) | (1 << j)
+				probability += self.btc(hand_mask, enemy_mask, opened_mask)
+		probability *= mult
+		if probability > 0.75:
+			return self.__raise__(self.table_info.big_blind() * 4 + value)
+		elif probability > 0.25:
+			return self.__check_or_call__(value)
+		else:
+			if value > 0:
+				return self.__fold__()
+			else:
+				return self.__check_or_call__(value)
+
+	def turn(self, value):
 		number_cards_on_table = len(self.table_info.opened_cards())
 		if number_cards_on_table == 0:
-			return self.turn_preflop()
+			return self.turn_preflop(value)
 		elif number_cards_on_table == 3:
-			self.__check_or_call__()
+			return self.turn_flop(value)
 		elif number_cards_on_table == 4:
-			self.__check_or_call__()
+			return self.turn_turn(value)
 		elif number_cards_on_table == 5:
-			self.__check_or_call__()
+			return self.turn_river(value)
 
 
 #	def add_message(self, message):
